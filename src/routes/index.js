9bc22e89baef409a7f8009f9820984e4056c7deb
naufalnak <naufal.ndak17@gmail.com>
2024-11-30 22:21:19 +0700

    feat: add routes index etc

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import exampleRoutes from "./exampleRoutes.js";
+import userRoutes from "./userRoutes.js";
+import authRoutes from "./authRoutes.js";
 
 const configureRoutes = (app) => {
-  app.use('/api/example', exampleRoutes);  
+  app.use("/api/example", exampleRoutes);
+  app.use("/api", userRoutes);
+  app.use("/api/auth", authRoutes);
 
   // Dilanjutkan dengan endpoint anda seterusnya
 };
