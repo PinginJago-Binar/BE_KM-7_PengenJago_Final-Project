8958b6fec76e6d0d29e5e33e27727a2328764957
nazladiohevin <nazlahevinbusiness@gmail.com>
2024-12-05 01:45:12 +0700

    fix(seed): error on seat seed

--- src/seeding/seeds/seats.js
+++ src/seeding/seeds/seats.js
@@ -6,16 +6,15 @@ const seedSeats = async () => {
 
   const statuses = ["available", "booked"];
 
-  const seats = airplanes.map((airplane, airplaneIndex) => {
-    const seats = [];
+  const seats = [];
+  airplanes.forEach((airplane, airplaneIndex) => {    
     for (let i = 1; i <= airplane.totalSeat; i++) {
       seats.push({
         code: `${airplaneIndex + 1}${String.fromCharCode(64 + i)}`,
         airplaneId: airplaneIndex + 1,
         status: statuses[Math.floor(Math.random() * statuses.length)],
       });
-    }
-    return seats;
+    }    
   });
   
 
